3931d87de10ca49432e266260b7af2ef92288acb
TannerTX <tanner_bibb@yahoo.com>
2023-02-09 01:43:10 -0600

    Added keyboard interrupt

--- Assign1/server/00-networks-program1-Wserver-skeleton.py
+++ Assign1/server/00-networks-program1-Wserver-skeleton.py
@@ -11,30 +11,36 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 while True:
-	#Establish the connection
-	print('Ready to serve...')
-	connectionSocket, addr = serverSocket.accept()
-	try:
-		message = connectionSocket.recv(1024).decode()
-		filename = message.split()[1]
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
 
-		if filename == "/stop":
-			break
+        if filename == "/stop":
+            break
 
-		f = open(filename[1:])
-		outputdata = f.read()
-		#Send one HTTP header line into socket
-		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-		#Send the content of the requested file to the client
-		for i in range(0, len(outputdata)):
-			connectionSocket.send(outputdata[i].encode())
-		connectionSocket.send("\r\n".encode())
-		connectionSocket.close()
-	except IOError:
-		#Send response message for file not found
-		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-		connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
-		#Close client socket
-		connectionSocket.close()
+        f = open(filename[1:])
+        outputdata = f.read()
+        #Send one HTTP header line into socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        #Send response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        #Close client socket
+        connectionSocket.close()
+    
+    except KeyboardInterrupt:
+        serverSocket.close()
+        sys.exit()
+
+
 serverSocket.close()
 sys.exit()
